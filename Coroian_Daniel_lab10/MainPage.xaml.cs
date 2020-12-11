4012892830f83da56f9fbbfb4449a3872beb12c5
CoroDaniel <danielcoroian15@gmail.com>
2020-12-11 17:01:04 +0200

    Add project files.

---
+++ Coroian_Daniel_lab10/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Coroian_Daniel_lab10.Models;
+
+namespace Coroian_Daniel_lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
